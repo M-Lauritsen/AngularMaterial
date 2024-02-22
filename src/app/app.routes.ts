1d16bf31375581982c0cac2c356ffdb76125d359
Michael Lauritsen <i3028841@gmail.com>
2024-02-22 23:43:11 +0100

    added panel and fixed styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TableComponent } from './components/table/table.component';
 import { HomeComponent } from './components/home/home.component';
+import { ExpansionPanelComponent } from './components/expansion-panel/expansion-panel.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'table', component: TableComponent },
+  { path: 'expansion-panel', component: ExpansionPanelComponent },
 ];
