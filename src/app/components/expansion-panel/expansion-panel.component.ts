f3dafd559907acadc2c892181447fd6678b52c4c
Michael Lauritsen <i3028841@gmail.com>
2024-02-22 23:46:25 +0100

    added table component

--- src/app/components/expansion-panel/expansion-panel.component.ts
+++ src/app/components/expansion-panel/expansion-panel.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-expansion-panel',
   standalone: true,
-  imports: [MatExpansionModule],
+  imports: [MatExpansionModule, TableComponent],
   templateUrl: './expansion-panel.component.html',
   styleUrl: './expansion-panel.component.scss',
 })
