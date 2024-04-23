203a25f273b540ed0a9a437f30ad35f7a81674b4
Michael Lauritsen <i3028841@gmail.com>
2024-04-23 23:22:29 +0200

    updated chips

--- src/app/components/chips/chips.component.ts
+++ src/app/components/chips/chips.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-chips',
   standalone: true,
-  imports: [],
+  imports: [MatChipsModule],
   templateUrl: './chips.component.html',
-  styleUrl: './chips.component.scss'
+  styleUrl: './chips.component.scss',
 })
 export class ChipsComponent {
+  availableChips: ChipsColor[] = [
+    { name: 'none', color: undefined },
+    { name: 'accent', color: 'accent' },
+    { name: 'primary', color: 'primary' },
+    { name: 'warn', color: 'warn' },
+  ];
 
+  schemaList: SchemaList[] = [
+    { name: 'Fast', url: 'Fast/', color: 'warn' },
+    { name: 'Slow', url: 'Slow/', color: 'primary' },
+    { name: 'Meh', url: 'Meh/', color: 'accent' },
+  ];
+
+  LogChip(name: any) {
+    console.log(name);
+  }
+}
+export interface ChipsColor {
+  name: string;
+  color: ThemePalette;
+}
+
+export interface SchemaList {
+  name: string;
+  url: string;
+  color: ThemePalette;
 }
