2d3f8819bb0f430b3222400180d02d6ea7e03001
Michael Lauritsen <i3028841@gmail.com>
2024-05-09 21:23:41 +0200

    minor styling

--- src/app/components/expansion-panel/expansion-panel.component.ts
+++ src/app/components/expansion-panel/expansion-panel.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { StandardTableComponent } from '../table/standard-table/standard-table.component';
+import { TreeTableComponent } from '../table/tree-table/tree-table.component';
 
 @Component({
   selector: 'app-expansion-panel',
   standalone: true,
-  imports: [MatExpansionModule, StandardTableComponent],
+  imports: [MatExpansionModule, TreeTableComponent],
   templateUrl: './expansion-panel.component.html',
   styleUrl: './expansion-panel.component.scss',
 })
